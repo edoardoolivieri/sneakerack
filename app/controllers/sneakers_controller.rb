9ac205c4348ac139387f563331a6ff46bd852984
Edoardo Olivieri <diddo.42@hotmail.it>
2019-03-25 11:46:12 +0000

    fixed search

--- app/controllers/sneakers_controller.rb
+++ app/controllers/sneakers_controller.rb
@@ -1,12 +1,13 @@
 class SneakersController < ApplicationController
   def index
     if params[:query].present?
-       @sneakers = Sneaker.where("name LIKE ?", "%#{params[:query]}%")
+      sneakers_name = params[:query].downcase
+      @sneakers = Sneaker.where("name LIKE ?", "%#{sneakers_name}%")
       # sql_query = "name LIKE :query OR syllabus LIKE :query"
       # @sneakers = Sneaker.where(sql_query, query: "%#{params[:query]}%")
     else
-    @sneakers = Sneaker.all
-  end
+      @sneakers = Sneaker.all
+    end
   end
 
   def show

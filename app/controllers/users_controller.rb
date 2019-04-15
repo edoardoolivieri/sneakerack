7b01da991ed89527356a110b2285499f90e2fe13
Edoardo Olivieri <diddo.42@hotmail.it>
2019-04-15 17:41:39 +0100

    fixing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
    def show
-    @user = User.find(params[:id].to_i)
+    @user = User.find(params[:id])
    end
 end
